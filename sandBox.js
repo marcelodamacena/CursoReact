2c67dfeb8f88f62d703e2686e355b438b5d5eca0
damacena <cmtedamacena@icloud.com>
2021-07-01 20:01:13 -0300

    first commit

---
+++ sandBox.js
@@ -0,0 +1,195 @@
+
+// aqui foi visto String //
+/*let age = 24 
+let year = 2021
+let email = "marcelodamacena@live.com"
+let firtName = "Marcelo"
+let lastName = "Teixeira"
+
+
+let index = email.indexOf('@');
+//let index = email.lastIndexOf('a');
+
+
+//let fullName = firtName + ' ' +lastName;
+//console.log(fullName.length);
+
+//console.log(fullName.toUpperCase());
+//console.log(fullName.toLowerCase());
+
+let result = email.replace('m', '@@@@')
+console.log(result);*/
+
+// aqui começa os inteiros //
+
+//let radius = 11;
+//let pi = 5;
+
+// operators + - * / ** %
+//console.log(radius % pi );
+
+//let likes = 10 
+//likes = likes + 1;
+//likes++;
+//likes += 2
+
+
+//console.log (likes );
+
+//------------String 
+
+//let fullName = "marcelo Damacena"; 
+//let age = 34 ;
+// funcao template String.
+
+//let result = `Meu nome é ${fullName} e tenho ${age}`;
+
+//et names = ['marcelo', 'mario','ana'];
+//let ages = [1,34,99];
+
+//console.log(`${names[1]} tem ${ages[2]} anos`);
+
+
+//let result1 = names.join('-');
+
+//let result = names.indexOf('marcelo');
+
+
+//let result = names.concat(['joao', 'Felipe']);
+
+//names. push('joao', 'ana');
+//names.pop('');
+
+//console.log(names.join('-'));
+
+
+// ---------------------- boolean
+
+//console.log(true,false, 'true', 'false');
+
+//let email = "marcelodamacena@live.com";
+//let names = ['gabriel', 'ana'];
+
+//let result = email.includes('@');
+
+//let result = names.includes('ana');
+
+
+//let age = 34;
+
+//console.log(age === '24');
+//console.log(age !== '34');
+
+//console.log(age != 25);
+//console.log(age > 25);
+
+
+
+//------------------------ type conversion
+
+//let score = 100 != 33;
+
+//score = Number(score);
+
+//sscore = String(score);
+
+//console.log(typeof score, score );
+
+//let names = ['gabriel', 'ana'];
+
+//let result = Boolean (1);
+
+//if( names.length > 0 )
+
+//if ( names.length){
+
+//console.log('TRUE');
+//} else {
+//    console.log ('FALSE');
+//}
+
+//console.log(result, typeof result);
+
+///----------- loop de repetiçao 
+//let array = ['gabriel', 'ana', 'joao']
+
+/*for (let i = 0; i < array.length; i++) {
+    console.log("Index: " +array[i]);
+    
+}*/
+
+//let i = 1 ;
+
+//while (i <= 5){
+//    console.log(i)
+//    i++
+//}
+
+/*const password = 'pyeeessssss'
+
+if(password.length >= 8 && password.includes('@') ){
+    console.log('Senha é forte')
+}
+
+
+else {
+    console.log('A senha é fraca')
+}*/
+
+/*const scores =  [10,33,40,50,99,130]
+
+
+for (let i = 0; i < scores.length; i++)
+{
+    if(scores[i] == 40 ){
+        break;
+
+    }
+    console.log('Seu score é: ' + scores[i])
+
+}*/
+
+/*const key = 'E'
+
+
+switch (key) {
+
+    case 'A' :
+        console.log ('Sua letra : A')
+        break;
+        case 'B' :
+            console.log ('Sua letra : B')
+            break;
+            case 'C' :
+                console.log ('Sua letra : C')
+                break;
+                case 'D' :
+                    console.log ('Sua letra : D')
+                    break;
+                    default:
+                        console.log('Nao sei sua letra')
+            
+}*/
+
+/*let age = 24;
+
+
+if (true) {
+    console.log('Sua idade 1 é: ' +age )
+}
+console.log('Sua idade 2 é: ' +age )
+
+<ima*/
+
+
+//let name = '/^[a-zA-Z0-2]+$/';
+
+//console.log(name.split('a'));
+
+//console.log('/^[a-zA-Z0-2]+$/'.test());
+
+
+
+let name;
+
+console.log(name)
