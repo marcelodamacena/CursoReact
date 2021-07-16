2757baa54b6faaf196ebbef32fc1f84159588cf7
Marcelo Damacena <marcelodamacena@live.com>
2021-07-15 21:03:01 -0300

    Update ex18.js

--- Avaliação/ex18.js
+++ Avaliação/ex18.js
@@ -1,9 +1,6 @@
 //ex18
-let numero = 0;
-let total = 0;
-numero=prompt("Digite um numero inteiro entre 0 e 100");
+let numero=prompt("Digite um numero inteiro entre 0 e 100");
 
 for(let index = 0; index <= numero; index++) {
-    total = index;
-    console.log(total);
+    console.log(index);
 }
